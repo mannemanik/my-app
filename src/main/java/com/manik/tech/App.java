c91ad4ec6b6a3dd7ea8d29078f825117eb2ce80b
Manik <mannemanik@gmail.com>
2019-07-03 15:22:58 -0400

    created git project in maven project folder

---
+++ src/main/java/com/manik/tech/App.java
@@ -0,0 +1,13 @@
+package com.manik.tech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
